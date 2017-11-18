f73f7db958c69da9f112a2a7eda7ef74da6ca710
Tara Wilson <tarawilson@taras-mbp.home>
2017-11-18 13:10:59 -0500

    Initial Commit

---
+++ StageTransition/StageTransition.h
@@ -0,0 +1,19 @@
+//
+//  StageTransition.h
+//  StageTransition
+//
+//  Created by Tara Wilson on 11/18/17.
+//  Copyright © 2017 taraw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StageTransition.
+FOUNDATION_EXPORT double StageTransitionVersionNumber;
+
+//! Project version string for StageTransition.
+FOUNDATION_EXPORT const unsigned char StageTransitionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StageTransition/PublicHeader.h>
+
+
